00bbc0989985e98aab555c3fccaf664286cab5a0
dominique Taplin <Dtaplin21@aol.com>
2025-05-11 21:09:37 -0700

    added files

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CommandPalette } from '@/components/command/CommandPalette'
+
+export default function Home() {
+  return (
+    <main>
+      <CommandPalette />
+    </main>
+  )
+} 
